849a5b17a1855c5e85aaa02b014773fe9bee538b
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-01 06:45:09 +0700

    testing size list movie and tv

--- app/src/test/java/com/gunder/film/ui/home/content/ContentViewModelTest.kt
+++ app/src/test/java/com/gunder/film/ui/home/content/ContentViewModelTest.kt
@@ -1,10 +1,28 @@
 package com.gunder.film.ui.home.content
 
-import com.gunder.film.ui.detail.DetailViewModel
-import com.gunder.film.utils.DataDummy
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import org.junit.Assert.*
 
 class ContentViewModelTest {
+    private lateinit var viewModel: ContentViewModel
+
+    @Before
+    fun setUpTest() {
+        viewModel = ContentViewModel()
+    }
+
+    @Test
+    fun getListMovie() {
+        val movie = viewModel.getListMovie()
+        assertNotNull(movie)
+        assertEquals(10, movie.size)
+    }
+
+    @Test
+    fun getListTv() {
+        val tv = viewModel.getListTv()
+        assertNotNull(tv)
+        assertEquals(10, tv.size)
+    }
 }
