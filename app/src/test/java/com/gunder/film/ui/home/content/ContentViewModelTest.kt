b205e07a92d1b549c3c9ceaf199088c8b4613a4f
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-01 06:25:52 +0700

    unit testing detail movie dan tv

--- app/src/test/java/com/gunder/film/ui/home/content/ContentViewModelTest.kt
+++ app/src/test/java/com/gunder/film/ui/home/content/ContentViewModelTest.kt
@@ -1,5 +1,45 @@
 package com.gunder.film.ui.home.content
 
+import com.gunder.film.ui.detail.DetailViewModel
+import com.gunder.film.utils.DataDummy
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
 
-class ContentViewModelTest
+class ContentViewModelTest {
+    private lateinit var detailViewModel: DetailViewModel
+    private val dummyMovie = DataDummy.getDummyMovie()[0]
+    private val dummyTv = DataDummy.getdummyTv()[0]
+    private val movieId = dummyMovie.id
+    private val tvId = dummyTv.id
+
+    @Before
+    fun setUp() {
+        detailViewModel = DetailViewModel()
+        detailViewModel.setMovieId(movieId)
+        detailViewModel.setTvId(tvId)
+    }
+
+    @Test
+    fun getDetailMovie() {
+        val movie = detailViewModel.getDetailMovieById()
+        assertNotNull(movie)
+        assertEquals(dummyMovie.title, movie.title)
+        assertEquals(dummyMovie.description, movie.description)
+        assertEquals(dummyMovie.genre, movie.genre)
+        assertEquals(dummyMovie.releaseYear, movie.releaseYear)
+        assertEquals(dummyMovie.imgPoster, movie.imgPoster)
+        assertEquals(dummyMovie.imgBakground, movie.imgBakground)
+    }
+    @Test
+    fun getDetailTv() {
+        val tv = detailViewModel.getTvDetailById()
+        assertNotNull(tv)
+        assertEquals(dummyTv.title, tv.title)
+        assertEquals(dummyTv.description, tv.description)
+        assertEquals(dummyTv.genre, tv.genre)
+        assertEquals(dummyTv.releaseYear, tv.releaseYear)
+        assertEquals(dummyTv.imgPoster, tv.imgPoster)
+        assertEquals(dummyTv.imgBakground, tv.imgBakground)
+    }
+}
