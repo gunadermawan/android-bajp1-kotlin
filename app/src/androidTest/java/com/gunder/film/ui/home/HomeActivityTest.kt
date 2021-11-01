5e5edb93c0e4932c88ccec77e06922c6a90beaa6
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-01 08:26:29 +0700

    instumental testing loadMovie dan detailMovie

--- app/src/androidTest/java/com/gunder/film/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/gunder/film/ui/home/HomeActivityTest.kt
@@ -1,5 +1,56 @@
 package com.gunder.film.ui.home
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.gunder.film.utils.DataDummy
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.gunder.film.R
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class HomeActivityTest
+@RunWith(AndroidJUnit4::class)
+class HomeActivityTest {
+    private val dummyMove = DataDummy.getDummyMovie()
+//    private val dummyTv = DataDummy.getdummyTv()
+
+    @get:Rule
+    var activityRule = ActivityTestRule(HomeActivity::class.java)
+
+    @Test
+    fun loadMovie() {
+        onView(withId(R.id.rv_movie)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_movie)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyMove.size
+            )
+        )
+    }
+
+    @Test
+    fun loadDetailMovie() {
+        onView(withId(R.id.rv_movie)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_movie)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.tv_title_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_title_detail)).check(matches(withText(dummyMove[0].title)))
+        onView(withId(R.id.tv_description_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_description_detail)).check(matches(withText(dummyMove[0].description)))
+        onView(withId(R.id.tv_release_date_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_release_date_detail)).check(matches(withText(dummyMove[0].releaseYear)))
+        onView(withId(R.id.tv_genre_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_genre_detail)).check(matches(withText(dummyMove[0].genre)))
+        onView(withId(R.id.iv_item_photo_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.iv_cover_detail)).check(matches(isDisplayed()))
+    }
+}
