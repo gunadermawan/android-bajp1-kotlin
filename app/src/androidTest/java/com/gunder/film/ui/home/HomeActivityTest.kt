c14ddfd6787eaa485bbde8d8ded43cf26d4401fa
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-01 13:47:48 +0700

    membuat instrumental testing untuk loadTv dan loadDetailTv

--- app/src/androidTest/java/com/gunder/film/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/gunder/film/ui/home/HomeActivityTest.kt
@@ -9,6 +9,7 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.gunder.film.R
 import org.junit.Assert.*
 import org.junit.Rule
@@ -18,10 +19,10 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class HomeActivityTest {
     private val dummyMove = DataDummy.getDummyMovie()
-//    private val dummyTv = DataDummy.getdummyTv()
+    private val dummyTv = DataDummy.getdummyTv()
 
     @get:Rule
-    var activityRule = ActivityTestRule(HomeActivity::class.java)
+    var activityRule = ActivityScenarioRule(HomeActivity::class.java)
 
     @Test
     fun loadMovie() {
@@ -50,6 +51,39 @@ class HomeActivityTest {
         onView(withId(R.id.tv_release_date_detail)).check(matches(withText(dummyMove[0].releaseYear)))
         onView(withId(R.id.tv_genre_detail)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_genre_detail)).check(matches(withText(dummyMove[0].genre)))
+        onView(withId(R.id.iv_item_photo_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.iv_cover_detail)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun loadTv() {
+        onView(withText("TV")).perform(click())
+        onView(withId(R.id.rv_tv)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_tv)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyTv.size
+            )
+        )
+    }
+
+    @Test
+    fun loadDetailTv() {
+        onView(withText("TV")).perform(click())
+        onView(withId(R.id.rv_tv)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_tv)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.tv_title_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_title_detail)).check(matches(withText(dummyTv[0].title)))
+        onView(withId(R.id.tv_description_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_description_detail)).check(matches(withText(dummyTv[0].description)))
+        onView(withId(R.id.tv_release_date_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_release_date_detail)).check(matches(withText(dummyTv[0].releaseYear)))
+        onView(withId(R.id.tv_genre_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_genre_detail)).check(matches(withText(dummyTv[0].genre)))
         onView(withId(R.id.iv_item_photo_detail)).check(matches(isDisplayed()))
         onView(withId(R.id.iv_cover_detail)).check(matches(isDisplayed()))
     }
